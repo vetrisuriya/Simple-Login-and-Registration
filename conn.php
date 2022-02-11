b6da324ed8c0836c5869cbc6984fc1f03390e6f0
Vetri Suriya <34440110+vetrisuriya@users.noreply.github.com>
2022-02-11 23:10:11 +0530

    Add files via upload

--- conn.php
+++ conn.php
@@ -1,9 +1,9 @@
 <?php
 
-$dbname = "heroku_a3945330c99fc52";
-$userName = "bd6a8702036a66";
-$userPass = "4a9939fd";
-$connection = "mysql:host=us-cdbr-east-05.cleardb.net;dbname=".$dbname."";
+$dbname = "registration_system";
+$userName = "root";
+$userPass = "";
+$connection = "mysql:host=localhost;dbname=".$dbname."";
 
 try {
     $con = new PDO($connection, $userName, $userPass);
